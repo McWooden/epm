6606ffe57d86492fb53459e27171615de5d7b265
McWooden <garryf7887@gmail.com>
2024-08-17 23:10:06 +0700

    add 2 page and crawl it

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -5,6 +5,18 @@ export default function sitemap() {
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1
-    }
+    },
+    {
+        url: 'https://cveleganprimamandiri.vercel.app/cveleganprimamandiri',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.9
+    },
+    {
+        url: 'https://cveleganprimamandiri.vercel.app/cveleganprima',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.8
+    },
     ]
 }
