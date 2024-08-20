9f8f98149365aae42bc4c19b84cae840ba483d30
McWooden <garryf7887@gmail.com>
2024-08-20 10:43:55 +0700

    add prio oemar bakrie

---
+++ src/app/priyooemarbakrie/page.js
@@ -0,0 +1,9 @@
+import Templet from "../components/Templet"
+
+export const metadata = {
+    title: 'Priyo Oemar Bakrie',
+}
+
+export default function CvEleganPrimaMandiri() {
+  return <Templet/>
+}
