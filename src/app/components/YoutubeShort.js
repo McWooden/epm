2abc2492e4f7f91a92db93ad82370fe4b701e4bf
McWooden <garryf7887@gmail.com>
2024-09-01 00:36:37 +0700

    beeper funk

--- src/app/components/YoutubeShort.js
+++ src/app/components/YoutubeShort.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 export default function YoutubeShort() {
-    return <div className="flex gap-2">
-        <iframe className="h-32" width="315" height="560" src="https://www.youtube.com/embed/LryDdRfnQSQ" title="Elegan Transport" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div className="flex gap-2">
-            <Image src={'/assets/random/transport.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-32"/>
-            <Image src={'/assets/random/transport-2.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-32"/>
-        </div>
+    return <div className="flex gap-2 overflow-auto">
+        <iframe className="rounded-xl h-48 shadow" width="853" height="480" src="https://www.youtube.com/embed/EWmQdPm7zXg" title="Elegan Transport Watermark" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <Image src={'/assets/random/transport.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
+        <Image src={'/assets/random/transport-2.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
+        <Image src={'/assets/random/transport.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
+        <Image src={'/assets/random/transport-2.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
+        <Image src={'/assets/random/transport.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
+        <Image src={'/assets/random/transport-2.jpg'} alt={'transport'} width={500} height={500} className="rounded-xl h-48 shadow"/>
     </div>
 }
