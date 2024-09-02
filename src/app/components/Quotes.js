e795ba1c974b8d1aabcb08aa6597b167ab3d5c99
McWooden <garryf7887@gmail.com>
2024-09-03 00:45:58 +0700

    fix quotes

--- src/app/components/Quotes.js
+++ src/app/components/Quotes.js
@@ -4,7 +4,7 @@ import { GiForestCamp } from "react-icons/gi";
 
 export default function Quotes() {
     return <div className="flex gap-2 bg-gray-200 p-6 relative overflow-hidden">
-        <Image className="h-12 w-12 object-cover rounded-full" src={'/assets/other/biro-perjalanan-wisata.jpg'} alt={'pp-quote'} width={250} height={250}/>
+        <Image className="h-12 w-12 object-cover rounded-full" src={'/assets/other/biro-perjalanan-wisata.webp'} alt={'pp-quote'} width={250} height={250}/>
         <div className="flex flex-col gap-2">
             <p className="text-gray-800 text-2xl font-semibold flex gap-2 flex-wrap">Nikmatilah hari ini, besok kita cari cerita lagi.</p>
             <p className="text-gray-600 text-sm">masyok, 7 July 2024.</p>
