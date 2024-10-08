643653de9fdad81525e4056015a9e0fcfd7bea2d
McWooden <garryf7887@gmail.com>
2024-10-08 19:15:29 +0700

    tanda tanyanya dua

--- src/app/components/Youtube.js
+++ src/app/components/Youtube.js
@@ -1,9 +1,11 @@
 const sourceYoutube = [
-    {src: `https://www.youtube.com/embed/JlObLpanBHw?${process.env.NODE_ENV === 'production' && '?autoplay=1&mute=1'}`, title: 'Event Organizer'},
+    {src: `https://www.youtube.com/embed/JlObLpanBHw${process.env.NODE_ENV === 'production' && '?autoplay=1&mute=1'}`, title: 'Event Organizer'},
     {src: "https://www.youtube.com/embed/45zTd_6nw7k", title: 'Wisata'},
     {src: "https://www.youtube.com/embed/EWmQdPm7zXg", title: 'Elegan Transport'},
     {src: "https://www.youtube.com/embed/-uIY7_6kpTo", title: 'Elegan Prima Mandiri'},
 ]
+
+console.log(process.env.NODE_ENV)
 
 export default function Youtube() {
     return <div className="flex gap-2 overflow-auto w-full py-6">
