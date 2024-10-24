3ce59f019ddd44610945d1ee191bf63b2b2db875
McWooden <garryf7887@gmail.com>
2024-10-25 04:55:23 +0700

    fade youtube

--- src/app/components/Youtube.js
+++ src/app/components/Youtube.js
@@ -12,9 +12,9 @@ export default function Youtube() {
             <div className="flex sm:w-1/3">
                 <iframe className="rounded-xl h-52 sm:h-64 shadow w-full" width="853" height="480" src={`https://www.youtube.com/embed/JlObLpanBHw${process.env.NODE_ENV === 'production' ? '?autoplay=1&mute=1' : ''}`} title={'Event Organizer'} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
-            <div className="flex flex-col gap-2 sm:w-1/3 text-gray-800">
-                <h3 className="font-semibold">Elegan Event | Bappeda - Litbangda kab. Magelang</h3>
-                <p className="line-clamp-4 text-sm text-ellipsis">Selamat datang di highlight seru dari acara yang diorganisir oleh Elegan Event bersama Bappeda-Litbangda kab. Magelang! Kami dengan penuh semangat menangani segala detail. Elegan Event selalu siap memberikan sentuhan unik dan kreatif untuk setiap event, membuatnya jadi kenangan manis bagi semua yang terlibat!</p>
+            <div className="flex flex-col gap-2 sm:w-1/3 ">
+                <h3 className="font-semibold text-gray-700">Elegan Event | Bappeda - Litbangda kab. Magelang</h3>
+                <p className="line-clamp-4 text-sm text-ellipsis text-gray-600">Selamat datang di highlight seru dari acara yang diorganisir oleh Elegan Event bersama Bappeda-Litbangda kab. Magelang! Kami dengan penuh semangat menangani segala detail. Elegan Event selalu siap memberikan sentuhan unik dan kreatif untuk setiap event, membuatnya jadi kenangan manis bagi semua yang terlibat!</p>
             </div>
         </div>
         <div className="flex gap-2 overflow-auto ">
